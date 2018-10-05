f1cf1a5242f57ac4c25d971f29e8a44aa634dc0e
Struan Judd <git@scifi.geek.nz>
2018-10-05 22:50:43 +1300

    Support Python 3.7

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description="A python package for script (and command) virtualisation with less typing.",
     entry_points={
